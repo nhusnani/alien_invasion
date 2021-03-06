2e0c47de5a1cebbd790a57eeba1aacf219e4744d
Noureen Husnani <Noureen_Husnani1@baylor.edu>
2021-03-06 02:27:21 -0600

    Moving Right

--- game_functions.py
+++ game_functions.py
@@ -122,4 +122,6 @@ def create_fleet(ai_settings, screen, ship, aliens):
             create_alien(ai_settings, screen, aliens, alien_number, 
                         row_number)
 
-        
+def update_aliens(aliens):
+    """Update the postions of all aliens in the fleet."""
+    aliens.update()
