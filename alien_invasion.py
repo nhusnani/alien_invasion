f2ce8163579dc6bdf2445a9f1a3d9c5045f52669
Noureen Husnani <Noureen_Husnani1@baylor.edu>
2021-03-05 21:46:01 -0600

    Both left and right ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,7 +18,7 @@ def run_game():
     # Set the background color.
     bg_color = (230, 230, 230)
 
-    # Start the main loop for the game.
+        # Start the main loop for the game.
     while True:
         gf.check_events(ship)
         ship.update()
--- game_functions.py
+++ game_functions.py
@@ -2,7 +2,7 @@ import sys
 
 import pygame
 
-def check_events():
+def check_events(ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -10,11 +10,17 @@ def check_events():
 
         elif event.type == pygame.KEYDOWN:   #Each keypress is registered with KD
             if event.key == pygame.K_RIGHT: #Check if key is right key
+                ship.rect.centerx += 1
                 ship.moving_right = True
+            elif event.key == pygame.K_LEFT: 
+                ship.moving_left = True
             
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
+                #ship.rect.centerx += 1
                 ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
