ff1c7bc642414e4660cc278db03f18a41a84fdad
Noureen Husnani <Noureen_Husnani1@baylor.edu>
2021-03-06 17:59:51 -0600

    Making Scoreboard

--- game_functions.py
+++ game_functions.py
@@ -71,7 +71,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         ship.center_ship()
            
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -83,6 +83,9 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
 
     # When you call draw() on a group, Pygame automatically draws each element
     # in the group at the position defined by its rect attribute.
+
+    # Draw the score information.
+    sb.show_score()
 
     #Draw the play button if the game is inactive.
     if not stats.game_active:
