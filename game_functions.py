d49142d58ec40c674ec716c24b4e59d714276a5c
Noureen Husnani <Noureen_Husnani1@baylor.edu>
2021-03-06 02:49:18 -0600

    Refactoring update bullets

--- game_functions.py
+++ game_functions.py
@@ -84,6 +84,13 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
         # up the for loop, which enables us to modify bullets inside the loop.
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """Respond to bullet-alien collisions."""
+    # Remove any bullets and aliens that have collided.
+
     # Check for any bullets that have hit aliens.
     # If so, get rid of the bullet and the alien.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
